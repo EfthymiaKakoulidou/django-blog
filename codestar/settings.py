3dde3441fb8ba522b3867687c2d412aaf05f68cf
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-02-10 20:42:54 +0000

    Add secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-um_+n49h!unsl4a(ogxub8=h1zg!!d5#4w7@6shz$f_l%st-1b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -131,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://noqbxbdo:2Z1C0mkv921ugWtjdgrCbMV0HJWQW2lc@mouse.db.elephantsql.com/noqbxbdo")
+
+os.environ.setdefault("SECRET_KEY", "12345")
