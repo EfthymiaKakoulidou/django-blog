5d22f9bb110ef6a5dc1a184f5b7fc29fec4086b2
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-02-12 14:08:19 +0000

    Update all files

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -25,10 +31,14 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`blog.Post`.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -37,4 +47,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
--- env.py
+++ env.py
@@ -1,6 +1,8 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgres://noqbxbdo:2Z1C0mkv921ugWtjdgrCbMV0HJWQW2lc@mouse.db.elephantsql.com/noqbxbdo")
-
-os.environ.setdefault("SECRET_KEY", "12345")
+    "DATABASE_URL", "<your-database-URL>")
+os.environ.setdefault(
+    "SECRET_KEY", "(j(j_+@%s$a955-5gw=m@b-%#$slmv$0aixrck&odnq*h+ig@0")
+os.environ.setdefault(
+    "CLOUDINARY_URL", "<cloudinary-URL>")
